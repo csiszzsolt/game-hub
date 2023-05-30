103084e94daab65774b66b8c56c3997f06b30cfa
Csiszer <zsolt.csiszer@platinaxe.com>
2023-05-30 19:22:21 +0300

    Equal card height

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card height="100%">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack marginBottom={3} justifyContent="space-between">
